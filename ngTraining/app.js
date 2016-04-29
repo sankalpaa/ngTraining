1b92aeee359b045b23ae1f2b46aaaa2d67b7555e
guththila2 <guththiladev02@gmail.com>
2016-04-29 22:32:16 +0530

    Speakers get from public api Directive

---
+++ ngTraining/app.js
@@ -0,0 +1,24 @@
+﻿var training = angular.module('trainingApp', ['ngRoute']);
+training.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/training', {
+            templateUrl: 'training.html',
+            controller:'trainingController'
+        })
+    .when('/speakers', {
+        templateUrl: 'speakers.html',
+        controller: 'speakerController'
+    });
+}
+]);
+
+training.directive('listItem', function() {
+    return {
+        restrict: 'E',
+        scope : {
+          name : '@'  
+        },
+        template : '<b><i>{{name}}</i></b>'
+    }
+});
+
+
---
+++ ngTraining/speakers.js
@@ -0,0 +1,6 @@
+﻿training.controller('speakerController', function ($scope,$http) {
+
+    $http.get('http://jsonplaceholder.typicode.com/users').success(function (data) {
+            $scope.speakers = data;
+        });
+});
