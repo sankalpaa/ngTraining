560d4d46ea6f1523bde2381e0f9e3404dda0bbc5
guththila2 <guththiladev02@gmail.com>
2016-04-29 18:10:52 +0530

    Add solution and initial training page

---
+++ ngTraining/training.js
@@ -0,0 +1,10 @@
+﻿var training = angular.module('trainingApp', []);
+training.controller('trainingController', function ($scope) {
+    $scope.trainings = [];
+    $scope.add = function (name, duration) {
+        $scope.trainings.push({ Name: name, Duration: duration });
+        $scope.training.name = '';
+        $scope.training.duration = 4;
+
+    }
+});
